c829344594bec4347480b065a7dcdfe41ce11b98
Evan-cse <evanshareef@gmail.com>
2023-10-14 21:25:29 +0600

    Initial commit

---
+++ src/AnimatedBackground/AnimatedBackground.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import image1 from '../Resources/1.jpeg';
+import image2 from '../Resources/2.jpeg';
+import image3 from '../Resources/3.jpeg';
+import './AnimatedBackground.css'
+
+
+const AnimatedBackground = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const imageArr                        = [image1, image2, image3];
+    const [isHovered, setIsHovered]       = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    const backgroundSliderStyle = {
+        padding: '80px',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundImage: `url(${imageArr[currentIndex]})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
+        zIndex: -1,
+        transition: '1s cubic-bezier(.19,.11,0,1.02)'
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setCurrentIndex((prevIndex) => (prevIndex + 1) % imageArr.length);
+        }, 3000);
+    
+        return () => clearInterval(interval);
+      }, []);
+
+    return(
+        <div 
+             style        = {backgroundSliderStyle}
+             onMouseEnter = {handleMouseEnter}
+             onMouseLeave = {handleMouseLeave}
+        >
+            <div>
+                <h1 className = "headingTag">
+                    Hey Folks. <br/>
+                    This Website will be Live Soon!
+                </h1>
+                <p className = "text-white text-justify" style = {{textAlign: 'left'}}>
+                    Fresh design and content is preparing right now. <br/>
+                    Don't forget to subscribe to stay tuned!
+                </p>
+            </div>
+            <div className = "row">
+                <div className = "col-md-6 col-sm-6">
+                    <form className = "form form-inline">
+                        <div className = "form-group">
+                            <input type = "email" className = "form-control" name = "email" id = "subscribe-email" placeholder = "Email Address" required = "" />
+                        </div>
+                        <button type = "submit" className = "btn btn-default btn-border btn-primary">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AnimatedBackground;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AnimatedBackground from './AnimatedBackground/AnimatedBackground';
+
+function App() {
+  return (
+    <div className="App">
+      <AnimatedBackground></AnimatedBackground>
+    </div>
+  );
+}
+
+export default App;
